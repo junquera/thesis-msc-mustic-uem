d1a66a584503525f86cd152376788ebcdb4089c0
J. Junquera <javier.junquera.sanchez@protonmail.com>
2019-08-20 11:03:29 +0200

    LWE!

--- lwe_tests/test_lwe.py
+++ lwe_tests/test_lwe.py
@@ -1,8 +1,21 @@
 import random
 
+'''
+junquera@opa:~/Dropbox/Estudios/UEM/TFM/it3/tfm/lwe_tests$ python3 test_lwe.py
+x > 1
+c1:  [20879, 467127, 239889, 298626, 143587, 116876, 441870, 358117, 405978, 117286]
+c2:  [862895, 409484, 878468, 595170, 591475, 813510, 498679, 582178, 793386, 539857]
+Result:  [340517, 340521, 340515]
+junquera@opa:~/Dropbox/Estudios/UEM/TFM/it3/tfm/lwe_tests$ python3 test_lwe.py
+x > 0
+c1:  [245223, 483493, 619825, 4950, 167883, 200668, 6545, 414358, 572824, 560333]
+c2:  [327111, 452061, 70139, 343482, 668823, 602074, 230847, 195368, 287346, 169525]
+Result:  [0, 9, 3]
+'''
+
 Z_MAX = 1e50
-q=Z_MAX
 MOD = 681029
+q=MOD
 DIMENSIONS = 10
 
 s = []
@@ -16,8 +29,8 @@ for i in range(100):
     a = []
     e = []
     for j in range(DIMENSIONS):
-        a.append(random.randint(0, Z_MAX))
-        e.append(random.randint(0, Z_MAX))
+        a.append(random.randint(0, Z_MAX) % MOD)
+        e.append(random.randint(0, 10))
     aes.append(a)
     es.append(e)
 
@@ -25,16 +38,21 @@ bs = []
 for i in range(len(aes)):
     b = []
     for j in range(DIMENSIONS):
-        b.append((aes[i][j]*s[j]+e[j])) # % MOD
+        b.append((aes[i][j]*s[j]+e[j]) % MOD) # % MOD
     bs.append(b)
 
 # TODO Probar para array de x
-x = 0
+x = int(input("x > "))
 
 # Cifrar
 v = []
 for i in range(DIMENSIONS):
-    v.append(random.randint(0, 1))
+    '''
+    Si v es aleatorio, queda delatada la b porque si x=0
+    y v=0 a=0 y b=x*q/2. v tiene que ser 1
+    '''
+    # v.append(random.randint(0, 1))
+    v.append(1)
 
 reses_a = []
 reses_b = []
@@ -51,11 +69,12 @@ for i in range(len(aes)):
 # Descifrar
 dec = []
 for i in range(len(reses_a)):
-    c2 = reses_b[i]
-    c1 = reses_a[i]
     for j in range(DIMENSIONS):
-        c1_aux = (c1[j]*s[j])
-        dec.append((c2[j] - c1_aux)) # % MOD
+        c1 = reses_a[i][j]
+        c2 = reses_b[i][j]
+        c1_aux = c1 * s[j]
+        dec.append((c2 - c1_aux) % MOD)
 
-for i in dec:
-    print(abs(abs(i))%MOD)
+print("c1: ", reses_a[0])
+print("c2: ", reses_b[0])
+print("Result: ", dec[:3])
